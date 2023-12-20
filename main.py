ceefdfdf49bed7316f55fd89529dfd2ae5624ac5
KuLiPoi <2272665567@qq.com>
2023-12-20 12:52:58 +0800

    All Module done.

--- main.py
+++ main.py
@@ -30,6 +30,38 @@ def json_check(json_data, target_text):
     return False
 
 
+# 对短信验证码进行解码
+def ocr_handler(self):
+    """
+    :param self: 打开的图片
+    :return: 返回解析后的验证码
+    """
+    host, _, _, username, password, soft_id, codetype, _, _ = config_handler()
+    ocr_url = f'{host}'
+    ocr_params = {
+        'user': username,
+        'pass': password,
+        'softid': soft_id,
+        'codetype': codetype,
+        'len_min': 6
+    }
+    ocr_header = {
+        'Connection': 'Keep-Alive',
+        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)',
+    }
+    files = {'userfile': ('Captcha.jpeg', self)}
+    ocr_resp = requests.post(ocr_url, data=ocr_params, headers=ocr_header, files=files, verify=False)
+    code_json = ocr_resp.json()
+    code = code_json['pic_str']
+    errmsg = code_json['err_str']
+    if errmsg != 'OK':
+        logger_stream.error('解析二维码出错，程序即将退出,服务器返回消息：' + errmsg)
+        exit()
+    else:
+        logger_stream.info('从第三方OCR服务器成功解析验证码：' + code)
+        return code
+
+
 # 配置文件读取
 def config_handler():
     config = configparser.ConfigParser()
@@ -64,39 +96,7 @@ def initialize():
             return True
         except requests.exceptions.RequestException as e:
             logger_stream.error(f'同步配置失败，请检查网络连接或反馈给开发者, {e}')
-            return False
-
-
-# 对玄心验证码进行解码
-def ocr_handler(self):
-    """
-    :param self: 打开的图片
-    :return: 返回解析后的验证码
-    """
-    host, _, _, username, password, soft_id, codetype, _, _ = config_handler()
-    ocr_url = f'{host}'
-    ocr_params = {
-        'user': username,
-        'pass': password,
-        'softid': soft_id,
-        'codetype': codetype,
-        'len_min': 6
-    }
-    ocr_header = {
-        'Connection': 'Keep-Alive',
-        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)',
-    }
-    files = {'userfile': ('Captcha.jpeg', self)}
-    ocr_resp = requests.post(ocr_url, data=ocr_params, headers=ocr_header, files=files, verify=False)
-    code_json = ocr_resp.json()
-    code = code_json['pic_str']
-    errmsg = code_json['err_str']
-    if errmsg != 'OK':
-        logger_stream.error('解析二维码出错，程序即将退出,服务器返回消息：' + errmsg)
-        exit()
-    else:
-        logger_stream.info('从第三方OCR服务器成功解析验证码：' + code)
-        return code
+            return None
 
 
 # 获取guid
@@ -129,11 +129,11 @@ def get_guid(device_id, manuid):
         logger_logs.error('Guid or Scene Error', guid_json)
         logger_stream.error('致命错误，获取Guid失败了，请尝试重启程序，或使用程序目录下的/tools/DeviceGen.exe重新为您分配一个设备，'
                             '程序即将停止。')
-        exit()
+        return None
 
 
 # 进行短信验证码登录，并获取sessid
-def get_sessid(manu_id, device_id, guid, phone_number, scene):
+def get_main_key(manu_id, device_id, guid, phone_number, scene):
     # 获得验证码 Session
     send_sms_captcha_url = 'https://daoyu.sdo.com/api/userCommon/sendSmsCode'
     send_sms_captcha_params = {
@@ -225,7 +225,7 @@ def get_sessid(manu_id, device_id, guid, phone_number, scene):
             logger_stream.info('校验码和服务器核对失败，即将重试..')
             captcha_session = check_captcha_json['data']['captcha_session']
         elif 'return_message' in check_captcha_json and check_captcha_json[
-                                                            'return_message'] == '短信获取频繁，已被受限，请稍等再试':
+            'return_message'] == '短信获取频繁，已被受限，请稍等再试':
             logger_stream.info('触发服务器风控，将在24小时后重试')
             time.sleep(86400)
         else:
@@ -235,7 +235,7 @@ def get_sessid(manu_id, device_id, guid, phone_number, scene):
 
     # 输入短信验证码并校验
     sms_code_input = input("请输入短信验证码：")
-    sms_login_url = 'https://daoyu.sdo.com/api/userCommon/validateSmsCodeLogin?'
+    sms_login_url = 'https://daoyu.sdo.com/api/userCommon/validateSmsCodeLogin'
     sms_login_params = {
         'sms_code': sms_code_input,
         'phone': phone_number,
@@ -259,6 +259,7 @@ def get_sessid(manu_id, device_id, guid, phone_number, scene):
     sms_login_response = requests.get(sms_login_url, params=sms_login_params, headers=sms_login_header, verify=False)
     sms_login_json = sms_login_response.json()
     if 'USERSESSID' in sms_login_json['data'] and sms_login_json['data']['is_login'] == '1':
+        # DY_XXXX
         user_sessid = sms_login_json['data']['USERSESSID']
         nick_name = sms_login_json['data']['nickname']
         logger_stream.info(f'你好,{nick_name},目前一切正常...程序将继续执行剩余任务...')
@@ -267,45 +268,311 @@ def get_sessid(manu_id, device_id, guid, phone_number, scene):
     else:
         logger_stream.info('无法解析叨鱼Key，请将日志文件发送给开发者')
         logger_logs.error('sms_login_json: ' + sms_login_json)
-        exit()
+        return None
 
 
-# 获取账户信息
-def userinfo_handler(sessid, device_id, manuid, phone_number):
-    # 获取绑定的账号信息
-    account_info_url = 'https://yaoshi.sdo.com/fk/yaoshi/login?'
-    account_info_params = {
-        'sessionid': sessid,
-        'netflag': 'WIFI',
-        'sequence': '54',
-        'version': 'i.9.3.3',
-        'registerflag': '0',
-        'firstflag': '0',
-        'txzDeviceId': device_id,
-        'deviceModel': manuid,
-        'deviceName': 'iPhone',
-        'osType': '1',
-        'osVersion': '17.0',
-        'pushToken': 'v2:63f51e743a0bd2058133df274c4659965eb36df6f793468df4388734a28e9f5c',
-        'phone': phone_number,
-        'loginType': '2',
-        'isHttp': '0'
+# DaoyuTicket
+# step 1/4 Get flowid
+def get_flowid(manuid, deviceid, sessionid, show_username):
+    get_flowid_url = 'https://daoyu.sdo.com/api/thirdPartyAuth/initialize'
+    get_flowid_params = {
+        'media_channel': 'AppStore',
+        'device_os': 'iOS17.0',
+        'idfa': '00000000-0000-0000-0000-000000000000',
+        'circle_id': '854742',
+        'app_version': 'i.9.3.3',
+        'device_manuid': manuid,
+        'src_code': '8',
+        'clientId': 'qu_shop',
+        'appId': '6666',
+        'scope': 'get_account_profile',
+        'extend': '',
+        'device_id': deviceid
     }
-    account_info_header = {
-        'User-Agent': '%E5%8F%A8%E9%B1%BC/1 CFNetwork/1474 Darwin/23.0.0',
-        'Host': 'yaoshi.sdo.com',
-        'Accept-Encoding': 'gzip, deflate'
+    get_flowid_header = {
+        'authority': 'daoyu.sdo.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'content-type': 'application/json',
+        'accept': '*/*',
+        'user-agent': 'SdAccountKeyM/9.3.3 (iPhone; iOS 17.0; Scale/3.00)',
+        'accept-language': 'zh-Hans-CN;q=1, zh-Hant-CN;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
     }
-    account_info_response = requests.get(account_info_url, params=account_info_params, headers=account_info_header,
-                                         verify=False)
-    account_info_json = account_info_response.json()
-    sndaid_dist_str = account_info_json['data']['SndaIdList']
-    sndaid_list_dict = {}
-    pairs = sndaid_dist_str.split(',')
-    for pair in pairs:
-        key, value = pair.split(':')
-        sndaid_list_dict[key] = value
-    return sndaid_list_dict
+    get_flowid_cookies = {
+        'user_sessionid': sessionid,
+        'is_login': '1',
+        'show_username': show_username,
+    }
+    get_flowid_response = requests.get(get_flowid_url, params=get_flowid_params, headers=get_flowid_header,
+                                       cookies=get_flowid_cookies, verify=False)
+    get_flowid_json = get_flowid_response.json()
+    flowid = get_flowid_json['data']['flowId']
+    logger_stream.info('获取叨鱼票据中，获取浮动值成功 1/4.')
+    logger_logs.info('flowid: ' + flowid)
+    return flowid
+
+
+# step 2/4 Get accountID list
+def get_account_id_list(flowid, deviceid, manuid, sessionid, show_username):
+    get_account_id_list_url = 'https://daoyu.sdo.com/api/thirdPartyAuth/queryAccountList'
+    get_account_id_list_params = {
+        'flowId': flowid,
+        'idfa': '00000000-0000-0000-0000-000000000000',
+        'device_manuid': manuid,
+        'src_code': '8',
+        'circle_id': '854742',
+        'device_os': 'iOS17.0',
+        'media_channel': 'AppStore',
+        'app_version': 'i.9.3.3',
+        'device_id': deviceid
+    }
+    get_account_id_header = {
+        'authority': 'daoyu.sdo.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'content-type': 'application/json',
+        'accept': '*/*',
+        'user-agent': 'SdAccountKeyM/9.3.3 (iPhone; iOS 17.0; Scale/3.00)',
+        'accept-language': 'zh-Hans-CN;q=1, zh-Hant-CN;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+    }
+    get_account_id_list_cookies = {
+        'user_sessionid': sessionid,
+        'is_login': '1',
+        'show_username': show_username,
+    }
+    get_account_id_list_response = requests.get(get_account_id_list_url, params=get_account_id_list_params,
+                                                headers=get_account_id_header,
+                                                cookies=get_account_id_list_cookies, verify=False)
+    get_account_id_list_json = get_account_id_list_response.json()
+    if get_account_id_list_json['return_message'] == 'success':
+        account_id_list = get_account_id_list_json['data']['accountList']
+        logger_stream.info(f'获取叨鱼票据中，获取账户全部信息成功 当前账户下存在{len(account_id_list)}个账户 2/4.')
+        return account_id_list
+    else:
+        logger_stream.info(f'获取叨鱼票据中，获取账户信息失败 2/4.')
+        logger_logs.info(f'Get accountList error，{get_account_id_list_json}。')
+        return None
+
+
+# step 3/4 Confirm with server
+def make_confirm(account_id, flowid, deviceid, manuid, sessionid, show_username):
+    make_confirm_url = 'https://daoyu.sdo.com/api/thirdPartyAuth/chooseAccount'
+    make_confirm_header = {
+        'authority': 'daoyu.sdo.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'content-type': 'application/json',
+        'accept': '*/*',
+        'user-agent': 'SdAccountKeyM/9.3.3 (iPhone; iOS 17.0; Scale/3.00)',
+        'accept-language': 'zh-Hans-CN;q=1, zh-Hant-CN;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+    }
+    make_confirm_params = {
+        'idfa': '00000000-0000-0000-0000-000000000000',
+        'flowId': flowid,
+        'device_os': 'iOS17.0',
+        'media_channel': 'AppStore',
+        'accountId': account_id,
+        'device_manuid': manuid,
+        'app_version': 'i.9.3.3',
+        'src_code': '8',
+        'device_id': deviceid,
+        'circle_id': '854742'
+    }
+    make_confirm_cookies = {
+        'user_sessionid': sessionid,
+        'is_login': '1',
+        'show_username': show_username,
+    }
+    make_confirm_response = requests.get(make_confirm_url, params=make_confirm_params,
+                                         headers=make_confirm_header, cookies=make_confirm_cookies, verify=False)
+    make_confirm_json = make_confirm_response.json()
+    confirm_message = make_confirm_json['return_message']
+    if confirm_message == 'success':
+        logger_stream.info(f'获取叨鱼票据步骤中，和服务器握手成功 3/4')
+        return True
+    else:
+        logger_stream.info(f'获取叨鱼票据步骤中，和服务器握手失败 3/4')
+        logger_logs.error(f'确认失败，原因：{confirm_message}')
+        return False
+
+
+# step 4/4 Get DaoyuTicket
+def get_sub_account_key(flowid, manuid, deviceid):
+    get_daoyu_ticket_url = 'https://daoyu.sdo.com/api/thirdPartyAuth/confirm'
+    get_daoyu_ticket_header = {
+        'authority': 'daoyu.sdo.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'content-type': 'application/json',
+        'accept': '*/*',
+        'user-agent': 'SdAccountKeyM/9.3.3 (iPhone; iOS 17.0; Scale/3.00)',
+        'accept-language': 'zh-Hans-CN;q=1, zh-Hant-CN;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+    }
+    get_daoyu_ticket_params = {
+        'app_version': 'i.9.3.3',
+        'flowId': flowid,
+        'idfa': '00000000-0000-0000-0000-000000000000',
+        'circle_id': '854742',
+        'device_manuid': manuid,
+        'media_channel': 'AppStore',
+        'device_os': 'iOS17.0',
+        'device_id': deviceid,
+        'src_code': '8'
+    }
+    get_daoyu_ticket_response = requests.get(get_daoyu_ticket_url, params=get_daoyu_ticket_params,
+                                             headers=get_daoyu_ticket_header, verify=False)
+    get_daoyu_ticket_json = get_daoyu_ticket_response.json()
+    if get_daoyu_ticket_json['return_code'] == 0:
+        daoyu_ticket = get_daoyu_ticket_json['data']['authorization']
+        logger_stream.info(f'获取叨鱼票据成功， 4/4.')
+        return daoyu_ticket
+    else:
+        logger_logs.error(f'Get_daoyuTicket error，{get_daoyu_ticket_json}')
+        logger_stream.info(f'获取叨鱼票据失败， 4/4.')
+        return None
+
+
+# get_temp_session ID
+def get_temp_sessionid(main_key):
+    get_temp_sessionid_url = ''
+    get_temp_sessionid_header = {
+        'authority': 'sqmallservice.u.sdo.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'qu-deploy-platform': '4',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'qu-merchant-id': '',
+        'qu-hardware-platform': '1',
+        'qu-software-platform': '2',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 12; 2206122SC Build/V417IR; wv) AppleWebKit/537.36 (KHTML, '
+                      'like Gecko) Version/4.0 Chrome/91.0.4472.114 Mobile Safari/537.36 DaoYu/9.4.8',
+        'qu-web-host': 'https://m.qu.sdo.com',
+        'origin': 'https://m.qu.sdo.com',
+        'x-requested-with': 'com.sdo.sdaccountkey',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://m.qu.sdo.com/',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+    get_temp_sessionid_params = {
+        'USERSESSID':main_key
+    }
+    get_temp_sessionid_response = requests.get(get_temp_sessionid_url, params=get_temp_sessionid_params,
+                                               headers=get_temp_sessionid_header, verify=False)
+    session_id = get_temp_sessionid_response.cookies.get('sessionId')
+    return session_id
+
+
+# Get sub_account_session
+def get_sub_account_session(sub_account_key):
+    get_sub_account_session_url = ''
+    get_sub_account_session_header = {
+        'authority': 'sqmallservice.u.sdo.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'qu-merchant-id': '1',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'qu-deploy-platform': '8',
+        'sec-fetch-site': 'same-site',
+        'qu-web-host': 'https://m.qu.sdo.com',
+        'accept-language': 'zh-CN,zh-Hans;q=0.9',
+        'sec-fetch-mode': 'cors',
+        'accept-encoding': 'gzip, deflate, br',
+        'origin': 'https://m.qu.sdo.com',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 DaoYu/9.3.3',
+        'referer': 'https://m.qu.sdo.com/',
+        'qu-hardware-platform': '2',
+        'qu-software-platform': '2',
+        'sec-fetch-dest': 'empty'
+    }
+    get_sub_account_session_params = {
+        'daoyuTicket': sub_account_key
+    }
+    get_temp_sessionid_response = requests.get(get_sub_account_session_url, params=get_sub_account_session_params,
+                                               headers=get_sub_account_session_header, verify=False)
+    session_id = get_temp_sessionid_response.cookies.get('sessionId')
+    return session_id
+
+
+# Do sign
+def do_sign(session_id):
+    sign_url = 'URL: https://sqmallservice.u.sdo.com/api/us/integration/checkIn'
+    sign_header = {
+        'authority': 'sqmallservice.u.sdo.com',
+        'method': 'PUT',
+        'scheme': 'https',
+        'content-length': '12',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'qu-web-host': 'https://m.qu.sdo.com',
+        'qu-hardware-platform': '1',
+        'qu-software-platform': '2',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 DaoYu/9.3.3',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'qu-deploy-platform': '4',
+        'qu-merchant-id': '1',
+        'origin': 'https://m.qu.sdo.com',
+        'x-requested-with': 'com.sdo.sdaccountkey',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://m.qu.sdo.com/',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+    sign_cookies = {
+        'sessionId': session_id
+    }
+    sign_response = requests.put(sign_url, headers=sign_header, cookies=sign_cookies, verify=False)
+    sign_json = sign_response.json()
+    if sign_json['resultMsg'] == 'SUCCESS':
+        return True
+    else:
+        return False
+
+
+# get balance
+def get_balance(session_id):
+    get_balance_url = 'https://sqmallservice.u.sdo.com/api/rs/member/integral/balance?merchantId=1'
+    get_balance_header = {
+        'authority': 'sqmallservice.u.sdo.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'qu-deploy-platform': '4',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'qu-merchant-id': '1',
+        'qu-hardware-platform': '1',
+        'qu-software-platform': '2',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 DaoYu/9.3.3',
+        'qu-web-host': 'https://m.qu.sdo.com',
+        'origin': 'https://m.qu.sdo.com',
+        'x-requested-with': 'com.sdo.sdaccountkey',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://m.qu.sdo.com/',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+    get_balance_cookies = {
+        'sessionId': session_id
+    }
+    get_balance_response = requests.get(get_balance_url, headers=get_balance_header, cookies=get_balance_cookies,
+                                        verify=False)
+    get_balance_json = get_balance_response.json()
+    balance = get_balance_json['data']['balance']
+    return balance
 
 
 # 主进程
