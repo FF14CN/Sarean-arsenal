98e3945bf7f6289dacc6edff1011bab7aa07323a
Cettiidae <li1048672037@gmail.com>
2024-01-04 19:01:18 +0800

    Optimize task execution logic

--- qinglong.py
+++ qinglong.py
@@ -11,24 +11,27 @@ from Utility.risingStones import signIn as rs_signin
 from Utility.risingStones import rs_login
 from Utility.risingStones import getSignReward as getSignReward
 from Utility.risingStones.rs_login import is_rs_login as rs_userInfo
+taskConfig = configparser.RawConfigParser()
+taskConfig.read('config.ini', encoding='utf-8')
+if taskConfig.get('Modules', 'sqMallTask') == 'True':
+    sqMallSign.main()
 
-sqMallSign.main()
+if taskConfig.get('Modules', 'risingStonesTask') == 'True':
+    noc_config = configparser.RawConfigParser()
+    noc_config.read('config.ini', encoding='utf-8')
+    if noc_config.get('Notification', 'noc-enable') == 'True':
+        import Utility.Notifications.push as pusher
 
-noc_config = configparser.RawConfigParser()
-noc_config.read('config.ini', encoding='utf-8')
-if noc_config.get('Notification', 'noc-enable') == 'True':
-    import Utility.Notifications.push as pusher
-
-    rs_cookies = rs_login.login()
-    rs_result = rs_signin.rs_signin(rs_cookies)
-    rs_reward = getSignReward.getReward(rs_cookies)
-    userInfo = rs_userInfo(rs_cookies)["msg"]
-    accountinfo = "当前登录账户:" + userInfo["character_name"] + "@" + userInfo["group_name"]
-    pusher.push('石之家任务结果', accountinfo + '\n' + str(rs_result) + '\n' + str(rs_reward))
-else:
-    rs_cookies = rs_login.login()
-    rs_result = rs_signin.rs_signin(rs_cookies)
-    rs_reward = getSignReward.getReward(rs_cookies)
-    userInfo = rs_userInfo(rs_cookies)["msg"]
-    accountinfo = "当前登录账户:" + userInfo["character_name"] + "@" + userInfo["group_name"]
-    print('石之家任务结果', accountinfo + '\n' + str(rs_result) + '\n' + str(rs_reward))
+        rs_cookies = rs_login.login()
+        rs_result = rs_signin.rs_signin(rs_cookies)
+        rs_reward = getSignReward.getReward(rs_cookies)
+        userInfo = rs_userInfo(rs_cookies)["msg"]
+        accountinfo = "当前登录账户:" + userInfo["character_name"] + "@" + userInfo["group_name"]
+        pusher.push('石之家任务结果', accountinfo + '\n' + str(rs_result) + '\n' + str(rs_reward))
+    else:
+        rs_cookies = rs_login.login()
+        rs_result = rs_signin.rs_signin(rs_cookies)
+        rs_reward = getSignReward.getReward(rs_cookies)
+        userInfo = rs_userInfo(rs_cookies)["msg"]
+        accountinfo = "当前登录账户:" + userInfo["character_name"] + "@" + userInfo["group_name"]
+        print('石之家任务结果', accountinfo + '\n' + str(rs_result) + '\n' + str(rs_reward))
