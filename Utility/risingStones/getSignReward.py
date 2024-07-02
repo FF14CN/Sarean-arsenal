9722202eddc00f54c3d04ba4cdb0ebe788f30e38
KuLiPoi <2272665567@qq.com>
2024-07-03 06:15:21 +0800

    Update getSignReward.py
    更新了签到奖励的获取逻辑，适配叨鱼登录的石之家。

--- Utility/risingStones/getSignReward.py
+++ Utility/risingStones/getSignReward.py
@@ -1,7 +1,13 @@
 from datetime import datetime
+<<<<<<< Updated upstream
 import time
 
+=======
+from Utility.risingStones import rs_login
+from Utility.risingStones import constant
+>>>>>>> Stashed changes
 import requests
+import time
 
 
 def getRewardStatus(cookies):
@@ -13,13 +19,24 @@ def getRewardStatus(cookies):
     RewardStatusurl = "https://apiff14risingstones.web.sdo.com/api/home/sign/signRewardList?month={}".format(
         currentTime)
     headers = {
+<<<<<<< Updated upstream
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/89.0.4389.90 Safari/537.36",
         "Cookie": cookies
+=======
+        **constant.RS_HEADERS_GET,
+        'authorization': daoyu_ticket,
+>>>>>>> Stashed changes
     }
     rewardStatus = requests.get(url=RewardStatusurl, headers=headers).json()
     signLogUrl = "https://apiff14risingstones.web.sdo.com/api/home/sign/mySignLog?month={}".format(currentTime)
+<<<<<<< Updated upstream
     signLogCount = requests.get(url=signLogUrl, headers=headers).json()["data"]["count"]
+=======
+    signLogCount = requests.get(url=signLogUrl, headers=headers, cookies=status_cookies).json()["data"]["count"]
+    if rs_login.debug:
+        print(rewardStatus)
+>>>>>>> Stashed changes
     if rewardStatus["code"] == 10000:
         return {
             "status": "success",
@@ -42,15 +59,31 @@ def getSignIDReward(cookies, rewardId,rewardMonth):
     """
     getRewardUrl = "https://apiff14risingstones.web.sdo.com/api/home/sign/getSignReward"
     headers = {
+<<<<<<< Updated upstream
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/89.0.4389.90 Safari/537.36",
         "Cookie": cookies
+=======
+        **constant.RS_HEADERS_GET,
+        'authorization': daoyu_ticket,
+>>>>>>> Stashed changes
     }
     payload = {
         "id": int(rewardId),
         "month": rewardMonth
     }
+<<<<<<< Updated upstream
     getReward = requests.post(url=getRewardUrl, headers=headers, data=payload).json()
+=======
+    if rs_login.debug:
+        print(payload)
+    get_signid_reward_cookies = {
+        'ff14risingstones': cookies
+    }
+    getReward = requests.post(url=getRewardUrl, headers=headers, data=payload, cookies=get_signid_reward_cookies).json()
+    if rs_login.debug:
+        print(getReward)
+>>>>>>> Stashed changes
     if getReward["code"] == 10000:
         return {
             "status": "success",
@@ -95,8 +128,13 @@ def getReward(cookies):
         today = datetime.now()
         for status in rewardStatus["data"]:
             if status["is_get"] == 0:
+<<<<<<< Updated upstream
                 time.sleep(10)
                 getResult = getSignIDReward(cookies, index,today.strftime("%Y-%m"))
+=======
+                time.sleep(50)
+                getResult = getSignIDReward(cookies, index, today.strftime("%Y-%m"), daoyu_ticket)
+>>>>>>> Stashed changes
                 returnMsg["data"][index - 1]["claimStatus"] = str(getResult)
             else:
                 claimStatus = f"奖励{index}不满足领取条件！或已领取！"
