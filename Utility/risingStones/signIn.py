b7e57df1e04893a9c5074871a6ffda676aea58d3
KuLiPoi <2272665567@qq.com>
2024-07-03 12:40:37 +0800

    Update signIn.py
    不知道之前的commit发生了什么...

--- Utility/risingStones/signIn.py
+++ Utility/risingStones/signIn.py
@@ -1,58 +1,33 @@
-<<<<<<< Updated upstream
-=======
 """
-Update: 2024-07-02
+Author: Cettiidae
+Version: 1.2.0
+Update Date: 2024-06-27
 """
->>>>>>> Stashed changes
 import requests
-
-"""
-签个到示范：
 from Utility.risingStones import rs_login
-<<<<<<< Updated upstream
-from Utility.risingStones import signIn as rs_signin
-print(rs_signin.rs_signin(rs_login.login()))
-#给默认账户签个到
-"""
-=======
 from Utility.risingStones import constant
->>>>>>> Stashed changes
 
 
-def rs_signin(cookies):
+def rs_signin(cookies,daoyu_ticket):
     """
     石之家签到组件
+    :param daoyu_ticket:
     :param cookies: 石之家cookies
     :return: status: success/fail
     """
     signinApi = "https://apiff14risingstones.web.sdo.com/api/home/sign/signIn"
     headers = {
-<<<<<<< Updated upstream
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
-=======
         **constant.RS_HEADERS_POST,
         'authorization': daoyu_ticket,
->>>>>>> Stashed changes
         "Cookie": cookies
     }
     signinResult = requests.post(url=signinApi, headers=headers)
     signinResult = signinResult.json()
-    print(signinResult)
     if signinResult["code"] == 10000:
-<<<<<<< Updated upstream
-        return {
-            "status": "success",
-            "message": signinResult["msg"],
-        }
-    else:
-        return {
-            "status": "fail",
-            "message": signinResult["msg"],
-        }
-=======
         if rs_login.debug:
             print(f'SignIn OK‘ {signinResult["msg"]}')
         msg = '签到成功 ~'
+        rs_login.logger_stream.info('签到成功了 ~')
     elif signinResult["code"] == 10001:
         if rs_login.debug:
             print(f'Already signed in‘ {signinResult["msg"]}')
@@ -61,6 +36,6 @@ def rs_signin(cookies):
         if rs_login.debug:
             print(f'SignIn Fail‘ {signinResult["msg"]}')
         msg = '签到失败 ~ 原因可能是盛趣的签到参数变动，请反馈给开发者。'
+        rs_login.logger_stream.info('签到失败了 ~')
         rs_login.logger_logs.error(signinResult["msg"])
     return msg
->>>>>>> Stashed changes
