650c30fd1270503830fdc450015dadbb6545d1d4
KuLiPoi <2272665567@qq.com>
2024-07-03 12:39:34 +0800

    Update run.py
    适配新版的DailyTask

--- run.py
+++ run.py
@@ -2,10 +2,7 @@
 import configparser
 
 from Utility.sqMall import sqMallDoSign as sqMallSign
-from Utility.risingStones import signIn as rs_signin
-from Utility.risingStones import rs_login
-from Utility.risingStones import getSignReward as getSignReward
-from Utility.risingStones.rs_login import is_rs_login as rs_userInfo
+from Utility.risingStones.dailytask import daily_task as lets_go
 
 taskConfig = configparser.RawConfigParser()
 taskConfig.read('config.ini', encoding='utf-8')
@@ -13,9 +10,4 @@ if taskConfig.get('Modules', 'sqMallTask') == 'True':
     sqMallSign.main()
 
 if taskConfig.get('Modules', 'risingStonesTask') == 'True':
-    rs_cookies = rs_login.login()
-    rs_result = rs_signin.rs_signin(rs_cookies)
-    rs_reward = getSignReward.getReward(rs_cookies)
-    userInfo = rs_userInfo(rs_cookies)["msg"]
-    accountinfo = "当前登录账户:" + userInfo["character_name"] + "@" + userInfo["group_name"]
-    print('石之家任务结果', accountinfo + '\n' + str(rs_result) + '\n' + str(rs_reward))
+    print(lets_go())
